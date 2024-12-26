3e6771d117d273deca6b39635185af83b82d992d
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2024-12-26 20:40:40 -0300

    corecao gitignore

---
+++ app/static/js/script-index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// script.js
+	
+  // Esconde o preloader
+  $("#preloader").fadeOut("slow", function () {
+    // Exibe o conteúdo após o preloader sumir
+    $("main").fadeIn("slow");
+  });
+	
+
+});
