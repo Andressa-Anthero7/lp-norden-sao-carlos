b2e4e6dff6ce06b4a78b75a9583dd26006c85583
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2024-12-27 02:20:56 -0300

    inclusao do front end da dashboard

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Leads, Config_WhatsApp
 
 # Create your views here.
 def index(request):
 	return render(request,'site/index.html')
+
+def dashboard(request,user):
+    leads = Leads.objects.all().order_by('-data_recebimento')
+    return render(request, 'site/dashboard.html', {'leads': leads})
